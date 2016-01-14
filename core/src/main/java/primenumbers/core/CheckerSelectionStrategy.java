c6f6f4a02de6a37598ddbe0fd3ccff7655527773
vavasilev <vavasilev@abv.bg>
2016-01-14 10:30:01 +0200

    Remove Unnecessary comments

--- core/src/main/java/primenumbers/core/CheckerSelectionStrategy.java
+++ core/src/main/java/primenumbers/core/CheckerSelectionStrategy.java
@@ -1,9 +1,3 @@
-/*  
- *  $ Id: $
- *  Copyright © 2016 Experian Ltd. All rights reserved.
- *  No Part of this file may be copied or distributed without the permission of Experian Ltd.
- */
-
 package primenumbers.core;
 
 import java.util.List;
